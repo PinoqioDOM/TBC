06fa8708dd00482f447c3c3e93cd00d65f3569b5
PinoqioDOM <alkhanishvilit@gmail.com>
2025-11-21 15:04:47 +0400

    Nav Bar and Design

---
+++ app/components/About.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Logo from "@/public/Logo.png"
+
+const About = () => {
+  return (
+    <div className="w-full"> 
+      
+      <div className="relative top-0 left-0 flex items-center">
+        <Image
+          src={Logo}
+          alt="Tbc Logo"
+          quality={100}
+          width={80}
+          priority
+        />
+        <h1 className="text-xl font-bold tracking-widest">მთავარია გჯეროდეს!</h1>
+      </div>
+
+      <div className="border-b border-white w-full"></div> 
+    </div>
+  );
+};
+
+export default About;
